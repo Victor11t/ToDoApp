4b91734535709fc6086af32b79b5faf5372c2882
victor lopez <lopezvictorfabian74@gmail.com>
2024-03-04 19:03:32 -0500

    final 9

--- todo/app.py
+++ todo/app.py
@@ -14,3 +14,34 @@ class Todo:
 
         if tag not in self.tags:
             self.tags.append(tag)
+
+    def __str__(self):
+        return f'{self.code_id} - {self.title}'
+
+
+class TodoBook:
+    def __init__(self):
+        self.todos = {}
+
+
+
+    def add_todo(self, title, description):
+        todo_id = len(self.todos) + 1
+        new_todo = Todo(todo_id, title, description)
+        self.todos[todo_id] = new_todo
+        return todo_id
+
+    def pending_todos(self):
+        return [todo for todo_id, todo in self.todos.items() if not todo.completed]
+
+    def completed_todos(self):
+        return [todo for todo_id, todo in self.todos.items() if todo.completed]
+
+    def tags_todo_count(self):
+        tag_count = {}
+        for todo in self.todos.values():
+            for tag in todo.tags:
+                if tag not in tag_count:
+                    tag_count[tag] = 0
+                tag_count[tag] += 1
+        return tag_count
